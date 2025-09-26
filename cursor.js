7bbb13948d263dea9aa6055f5925614ed6e41371
Olivia <158613870+Coneruu@users.noreply.github.com>
2025-09-26 21:51:43 +0100

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const cursor = document.getElementById('custom-cursor');
   document.addEventListener('mousemove', function (e) {
-    cursor.style.left = (e.clientX - 16) + 'px';
-    cursor.style.top = (e.clientY - 16) + 'px';
+    cursor.style.left = (e.clientX - 46) + 'px';
+    cursor.style.top = (e.clientY - 46) + 'px';
   });
 });
